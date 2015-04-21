147a01016267879640ab221b225e3a325ac267cd
ZucchiniZe <me@alexb.ninja>
2015-04-21 00:16:10 -0500

    oops

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Analytics
-!function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
-arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
-d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
-insertBefore(d,q)}(window,document,'script','_gs');
-
-_gs('GSN-473926-O');
-
 var t = new Trianglify();
 
 function draw() {
